954a87204badb09c7eceed95f9316655c1d4701a
ahmadouabed <48146739+ahmadouabed@users.noreply.github.com>
2024-10-22 05:45:31 +0800

    Create loginform.vue

---
+++ loginform.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="login-form">
+    <h2>Login</h2>
+    <form @submit.prevent="submitLogin">
+      <input v-model="username" type="text" placeholder="Username" required />
+      <input v-model="pin" type="password" placeholder="PIN" maxlength="4" required />
+      <button type="submit">Log In</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      username: '',
+      pin: ''
+    };
+  },
+  methods: {
+    submitLogin() {
+      this.$emit('login', { username: this.username, pin: this.pin });
+    }
+  }
+};
+</script>
+
+<style scoped>
+.login-form {
+  text-align: center;
+  margin-top: 100px;
+}
+</style>
